b972079a0f506d7de2e26294d05cb62155dee61f
Nicholas Dille <nicholas.dille@mailbox.org>
2021-05-04 15:15:54 +0200

    Initial commit

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -o errexit
+
+echo "### Building stack"
+docker build stacks/focal --target build --tag localhost:5000/nicholasdille/cnb-stack-build:focal --push
+docker build stacks/focal --target run   --tag localhost:5000/nicholasdille/cnb-stack-run:focal --push
+
+echo "### Building basic builder"
+pack builder create localhost:5000/nicholasdille/cnb-builder-basic:focal --config builder/basic/builder.toml --publish
+pack config default-builder localhost:5000/nicholasdille/cnb-builder-basic:focal
+
+echo "### Building buildpacks and builder"
+pack buildpack package localhost:5000/nicholasdille/cnb-buildpack-kubectl --config packages/kubectl/package.toml --publish
+pack builder create localhost:5000/nicholasdille/cnb-builder-tools:focal --config builder/tools/builder.toml --publish
+
+echo "### Building app"
+pack build localhost:5000/nicholasdille/cnb-test --builder localhost:5000/nicholasdille/cnb-builder-tools:focal --path app/test
